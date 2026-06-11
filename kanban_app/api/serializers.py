21ce716432dddd5a5a26de54b4d1ce660ec76ae4
Benjamin Blarr <b.blarr@gmx.de>
2026-06-11 13:34:26 +0200

    added Task model and made migration

--- kanban_app/api/serializers.py
+++ kanban_app/api/serializers.py
@@ -3,22 +3,6 @@ from kanban_app.models import Board
 from django.contrib.auth.models import User
 
 
-class BoardSerializer(serializers.ModelSerializer):
-    owner_id = serializers.PrimaryKeyRelatedField(read_only=True, 
-                                                  source='owner')
-    member_count = serializers.SerializerMethodField()
-    # ticket_count = 
-    # tasks_to_do_count = 
-    # tasks_high_prio_count = 
-
-    class Meta:
-        model = Board
-        fields = ['id', 'title', 'member_count', 'owner_id']
-
-    def get_member_count(self, obj):
-        return obj.members.count()
-    
-
 class UserSerializer(serializers.ModelSerializer):
 
     fullname = serializers.CharField(max_length=60, source='first_name')
@@ -28,6 +12,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'fullname']
 
+
+class BoardSerializer(serializers.ModelSerializer):
+    owner_id = serializers.PrimaryKeyRelatedField(read_only=True, 
+                                                  source='owner')
+    member_count = serializers.SerializerMethodField()
+    members = serializers.PrimaryKeyRelatedField(many=True, write_only=True, queryset=User.objects.all())
+    # ticket_count = 
+    # tasks_to_do_count = 
+    # tasks_high_prio_count = 
+
+    class Meta:
+        model = Board
+        fields = ['id', 'title', 'members' 'member_count', 'owner_id']
+
+    def get_member_count(self, obj):
+        return obj.members.count()
+    
 
 class BoardDetailReadSerializer(serializers.ModelSerializer):
     owner_id = serializers.PrimaryKeyRelatedField(read_only=True, 
--- kanban_app/models.py
+++ kanban_app/models.py
@@ -9,3 +9,31 @@ class Board(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('to-do' , 'to-do'),
+        ('in-progress' , 'in-progress'),
+        ('review' , 'review'),
+        ('done' , 'done'),
+    ]
+    PRIORITY_CHOICES = [
+        ('low' , 'low'),
+        ('medium' , 'medium'),
+        ('high' , 'high')
+    ]
+
+    board = models.ForeignKey(Board, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=200)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='to-do')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='low')
+    assignee = models.ForeignKey(User, related_name='assigned_tasks', null=True,blank=True, on_delete=models.SET_NULL)
+    reviewer = models.ForeignKey(User, related_name='review_tasks', null=True, blank=True, on_delete=models.SET_NULL)
+    due_date = models.DateField()
+    creator = models.ForeignKey(User, related_name='created_tasks', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
